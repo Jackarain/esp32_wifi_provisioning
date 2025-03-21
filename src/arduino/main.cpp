f35068b10428cab3be3692076920ea47d3091915
Jackarain <jack.wgm@gmail.com>
2025-03-21 12:38:28 +0800

    统一使用 TAG 进行日志记录，并为 Arduino 添加 NVS 初始化逻辑

--- src/arduino/main.cpp
+++ src/arduino/main.cpp
@@ -10,6 +10,9 @@
 #include <string.h>
 #include <unistd.h>
 
+#include <nvs_flash.h>
+#include <nvs.h>
+
 #include <esp_log.h>
 #include <esp_wifi.h>
 #include <esp_event.h>
@@ -24,7 +27,20 @@ static wifi_provisioning* g_wifi_provisioning = nullptr;
 
 void setup()
 {
-    ESP_LOGI("GUEST", "进入配置阶段");
+    const char* version = esp_get_idf_version();
+    ESP_LOGI(TAG, "*** 当前 idf 版本: %s ***", version);
+    ESP_LOGI(TAG, "*** 可用内存大小(bytes): %d ***", (int)esp_get_free_heap_size());
+
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        // NVS partition was truncated and needs to be erased retry nvs_flash_init
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
+
+    ESP_LOGI(TAG, "进入配置阶段");
 
     g_wifi_provisioning = new wifi_provisioning;
 
@@ -33,7 +49,7 @@ void setup()
         switch (status)
         {
         case wifi_status::CONNECTED:
-            ESP_LOGI("GUEST", "Wi-Fi 连接成功: %s", ssid.c_str());
+            ESP_LOGI(TAG, "Wi-Fi 连接成功: %s", ssid.c_str());
 
             // 连接成功释放资源.
             g_wifi_provisioning->stop();
@@ -41,7 +57,7 @@ void setup()
             g_wifi_provisioning = nullptr;
             break;
         case wifi_status::FAILED:
-            ESP_LOGE("GUEST", "Wi-Fi 连接失败: %s", ssid.c_str());
+            ESP_LOGE(TAG, "Wi-Fi 连接失败: %s", ssid.c_str());
             g_wifi_provisioning->start_config_server("ESP32-XXXX", "20121208");
             break;
         default:
@@ -53,15 +69,15 @@ void setup()
 
 void loop()
 {
-    ESP_LOGI("GUEST", "IP 地址: %s", g_wifi_provisioning->get_connected_ip().c_str());
+    ESP_LOGI(TAG, "IP 地址: %s", g_wifi_provisioning->get_connected_ip().c_str());
     static int count = 0;
-    ESP_LOGI("GUEST", "Count: %d", count++);
+    ESP_LOGI(TAG, "Count: %d", count++);
 
     if (++count == 2000 && g_wifi_provisioning)
     {
         g_wifi_provisioning->stop();
         delete g_wifi_provisioning;
         g_wifi_provisioning = nullptr;
-        ESP_LOGI("GUEST", "停止 Wi-Fi 配置");
+        ESP_LOGI(TAG, "停止 Wi-Fi 配置");
     }
 }
--- src/idf/main.cpp
+++ src/idf/main.cpp
@@ -22,7 +22,7 @@ static wifi_provisioning* g_wifi_provisioning = nullptr;
 
 extern "C" void setup()
 {
-    ESP_LOGI("GUEST", "进入配置阶段");
+    ESP_LOGI(TAG, "进入配置阶段");
 
     g_wifi_provisioning = new wifi_provisioning;
 
@@ -31,7 +31,7 @@ extern "C" void setup()
         switch (status)
         {
         case wifi_status::CONNECTED:
-            ESP_LOGI("GUEST", "Wi-Fi 连接成功: %s", ssid.c_str());
+            ESP_LOGI(TAG, "Wi-Fi 连接成功: %s", ssid.c_str());
 
             // 连接成功释放资源.
             g_wifi_provisioning->stop();
@@ -39,7 +39,7 @@ extern "C" void setup()
             g_wifi_provisioning = nullptr;
             break;
         case wifi_status::FAILED:
-            ESP_LOGE("GUEST", "Wi-Fi 连接失败: %s", ssid.c_str());
+            ESP_LOGE(TAG, "Wi-Fi 连接失败: %s", ssid.c_str());
             g_wifi_provisioning->start_config_server("ESP32-XXXX", "20121208");
             break;
         default:
@@ -51,15 +51,15 @@ extern "C" void setup()
 
 extern "C" void loop()
 {
-    ESP_LOGI("GUEST", "IP 地址: %s", g_wifi_provisioning->get_connected_ip().c_str());
+    ESP_LOGI(TAG, "IP 地址: %s", g_wifi_provisioning->get_connected_ip().c_str());
     static int count = 0;
-    ESP_LOGI("GUEST", "Count: %d", count++);
+    ESP_LOGI(TAG, "Count: %d", count++);
 
     if (++count == 2000 && g_wifi_provisioning)
     {
         g_wifi_provisioning->stop();
         delete g_wifi_provisioning;
         g_wifi_provisioning = nullptr;
-        ESP_LOGI("GUEST", "停止 Wi-Fi 配置");
+        ESP_LOGI(TAG, "停止 Wi-Fi 配置");
     }
 }
